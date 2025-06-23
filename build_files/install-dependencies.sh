29a6f2b80f2a4e6fe28cf2d1f001344b5293bf78
Blahkay <blakie1225@gmail.com>
2025-06-23 17:28:17 +1200

    Added ccache

--- build_files/install-dependencies.sh
+++ build_files/install-dependencies.sh
@@ -22,7 +22,7 @@ readonly TERRA_MESA_REPO="terra-mesa"  # Pre-exists in Bazzite
 
 # Package definitions
 declare -A PACKAGE_GROUPS=(
-    [ESSENTIAL]="git cmake gcc gcc-c++ make ninja-build autoconf automake libtool gettext gettext-devel pkgconf-pkg-config nasm yasm gperf swig python3-devel python3-pillow"
+    [ESSENTIAL]="git cmake gcc gcc-c++ make ninja-build autoconf automake libtool gettext gettext-devel pkgconf-pkg-config nasm yasm gperf swig python3-devel python3-pillow ccache"
 
     [CORE_DEPS]="alsa-lib-devel avahi-compat-libdns_sd-devel avahi-devel bzip2-devel curl dbus-devel fontconfig-devel freetype-devel fribidi-devel gawk giflib-devel gtest-devel libao-devel libass-devel libcap-devel libcdio-devel libcurl-devel libidn2-devel libjpeg-turbo-devel lcms2-devel libmicrohttpd-devel libmpc-devel libogg-devel libpng-devel libsmbclient-devel libtool-ltdl-devel libudev-devel libunistring libunistring-devel libusb1-devel libuuid-devel libvorbis-devel libxkbcommon-devel libxml2-devel libXmu-devel libXrandr-devel libxslt-devel libXt-devel lzo-devel mariadb-devel openssl-devel openssl-libs patch pcre-devel pcre2-devel pulseaudio-libs-devel sqlite-devel taglib-devel tinyxml-devel tinyxml2-devel trousers-devel uuid-devel zlib zlib-static zlib-devel systemd polkit rapidjson-devel hwdata hwdata-devel jre exiv2-devel json-devel meson ninja-build inotify-tools"
 
