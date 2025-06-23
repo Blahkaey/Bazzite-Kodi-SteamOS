fe4b64380e7af5bed34a0e2ed9a7a1c0cde18f9e
Blahkay <blakie1225@gmail.com>
2025-06-24 01:06:06 +1200

    Update install-dependencies.sh

--- kodi-base/scripts/install-dependencies.sh
+++ kodi-base/scripts/install-dependencies.sh
@@ -8,20 +8,4 @@ DNF_CMD="dnf5 -y --setopt=fastestmirror=1 --setopt=max_parallel_downloads=10 --s
 
 $DNF_CMD install git cmake gcc gcc-c++ make ninja-build autoconf automake libtool gettext gettext-devel pkgconf-pkg-config nasm yasm gperf swig python3-devel python3-pillow meson patch alsa-lib-devel avahi-compat-libdns_sd-devel avahi-devel bzip2-devel curl dbus-devel fontconfig-devel freetype-devel fribidi-devel gawk giflib-devel gtest-devel libao-devel libass-devel libcap-devel libcdio-devel libcurl-devel libidn2-devel libjpeg-turbo-devel lcms2-devel libmicrohttpd-devel libmpc-devel libogg-devel libpng12-devel libsmbclient-devel libtool-ltdl-devel libudev-devel libunistring libunistring-devel libusb1-devel libuuid-devel libvorbis-devel libxkbcommon-devel libxml2-devel libXmu-devel libXrandr-devel libxslt-devel libXt-devel lzo-devel mariadb-devel openssl-devel pcre-devel pcre2-devel pulseaudio-libs-devel sqlite-devel taglib-devel tinyxml-devel tinyxml2-devel trousers-devel uuid-devel zlib-devel rapidjson-devel hwdata-devel libdisplay-info libdisplay-info-devel libinput-devel mesa-libGLES-devel mesa-libgbm-devel mesa-libEGL-devel libdrm-devel libbluray-devel libcec-devel libnfs-devel libplist-devel shairplay-devel flatbuffers flatbuffers-devel fmt-devel fstrcmp-devel spdlog-devel jre bluez-libs-devel bluez-libs-devel json-devel libva-devel libvdpau-devel lirc-devel mesa-libGL-devel mesa-libGLU-devel mesa-libGLw-devel mesa-libOSMesa-devel openssl-libs
 
-cat > "/etc/yum.repos.d/fedora-41.repo" << EOF
-[fedora-41]
-name=Temporary fedora-41
-baseurl=https://download.fedoraproject.org/pub/fedora/linux/releases/41/Everything/x86_64/os/
-enabled=1
-gpgcheck=0
-priority=10
-EOF
-
-$DNF_CMD makecache --repo="fedora-41"
-
-
-$DNF_CMD install -y java-11-openjdk-headless --repo "fedora-41"
-
-rm -f "/etc/yum.repos.d/fedora-41.repo"
-
 echo "[INFO] Dependencies installed successfully"
